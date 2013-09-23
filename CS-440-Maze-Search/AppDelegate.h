b309b3f3a961a3d34eaf3e2b51392f103f1f2887
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-23 11:46:53 -0500

    Xcode Project Created

---
+++ CS-440-Maze-Search/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CS-440-Maze-SearchTests/CS_440_Maze_SearchTests.m
@@ -0,0 +1,34 @@
+//
+//  CS_440_Maze_SearchTests.m
+//  CS-440-Maze-SearchTests
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CS_440_Maze_SearchTests : XCTestCase
+
+@end
+
+@implementation CS_440_Maze_SearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
