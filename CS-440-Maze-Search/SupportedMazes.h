8120b0f1bcab2a7368ea28ea301dfdb36fec1e7c
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-23 23:04:00 -0500

    C function to return a list of the supported maze names

---
+++ CS-440-Maze-Search/SupportedMazes.h
@@ -0,0 +1,9 @@
+//
+//  SupportedMazes.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+NSArray * SupportedMazes();
---
+++ CS-440-Maze-Search/SupportedMazes.m
@@ -0,0 +1,13 @@
+//
+//  SupportedMazes.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "SupportedMazes.h"
+
+NSArray * SupportedMazes() {
+	return @[@"smallMaze", @"mediumMaze", @"bigMaze", @"openMaze"];
+}
