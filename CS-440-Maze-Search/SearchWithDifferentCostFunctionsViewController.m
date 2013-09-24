c50979d92b22d8217a3db3d50babd4fa76f8f2dd
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-23 23:03:37 -0500

    View Controllers for later sections

---
+++ CS-440-Maze-Search/SearchWithDifferentCostFunctionsViewController.m
@@ -0,0 +1,62 @@
+//
+//  SearchWithDifferentCostFunctionsViewController.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "SearchWithDifferentCostFunctionsViewController.h"
+#import "MazeManager.h"
+
+#define TAB_TITLE @"1.2"
+#define MAZE_PARSING_NOTIFICATION_NAME @"MazeParsingNotification"
+
+@interface SearchWithDifferentCostFunctionsViewController ()
+
+@end
+
+@implementation SearchWithDifferentCostFunctionsViewController {
+	NSArray *_mazes;
+}
+
+- (id)init {
+	self= [super init];
+	
+	if (self) {
+		// observe for maze parsing notifications
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updatedMazes:) name:MAZE_PARSING_NOTIFICATION_NAME object:nil];
+		
+		_mazes = [MazeManager sharedMazeManager].mazes;
+	}
+	
+	return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+	
+	[self.view setBackgroundColor:[UIColor lightGrayColor]];
+}
+
+#pragma mark - Updated Mazes Notification
+
+- (void)updatedMazes:(NSNotification *)notification {
+	NSNumber *updatedMazeIndex = [notification.userInfo objectForKey:@"updatedMazeIndex"];
+	// all entries in the list view should start off grayed out and remain untappable until the notification is received
+	_mazes = [notification.userInfo objectForKey:@"mazes"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
---
+++ CS-440-Maze-Search/SuboptimalSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuboptimalSearchViewController.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/23/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuboptimalSearchViewController : UIViewController
+
+@end
