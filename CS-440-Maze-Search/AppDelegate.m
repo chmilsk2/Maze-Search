d6c821fd6447b681ef8432289f874a6adea2fef5
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-23 22:55:35 -0500

    App delegate initializes a tabBarViewController that contains a navigationController per Assignment 1 section

--- CS-440-Maze-Search/AppDelegate.m
+++ CS-440-Maze-Search/AppDelegate.m
@@ -7,15 +7,53 @@
 //
 
 #import "AppDelegate.h"
+#import "BasicPathfindingViewController.h"
+#import "SearchWithDifferentCostFunctionsViewController.h"
+#import "SearchWithMultipleDotsViewController.h"
+#import "SuboptimalSearchViewController.h"
 
-@implementation AppDelegate
+#define BASIC_PATH_FINDING_TAB_BAR_TITLE @"1.1"
+#define SEARCH_WITH_DIFFERENT_COST_FUNCTIONS_TAB_BAR_TITLE @"1.2"
+#define SEARCH_WITH_MULTIPLE_DOTS_TAB_BAR_TITLE @"1.3"
+#define SUBOPTIMAL_SEARCH_TAB_BAR_TITLE @"1.4"
+
+@implementation AppDelegate {
+	UITabBarController *_tabController;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+	BasicPathfindingViewController *basicPathfindingViewController = [[BasicPathfindingViewController alloc] init];
+	SearchWithDifferentCostFunctionsViewController *searchWithDifferentCostFunctionsViewController = [[SearchWithDifferentCostFunctionsViewController alloc] init];
+	SearchWithMultipleDotsViewController *searchWithMultipleDotsViewController = [[SearchWithMultipleDotsViewController alloc] init];
+	SuboptimalSearchViewController *suboptimalSearchViewController = [[SuboptimalSearchViewController alloc] init];
+	
+	// view controllers
+	NSArray *viewControllers = @[basicPathfindingViewController, searchWithDifferentCostFunctionsViewController,searchWithMultipleDotsViewController, suboptimalSearchViewController];
+	
+	// tab bar titles
+	NSArray *tabBarTitles = @[BASIC_PATH_FINDING_TAB_BAR_TITLE, SEARCH_WITH_DIFFERENT_COST_FUNCTIONS_TAB_BAR_TITLE, SEARCH_WITH_MULTIPLE_DOTS_TAB_BAR_TITLE, SUBOPTIMAL_SEARCH_TAB_BAR_TITLE];
+
+	// navigation controllers
+	NSMutableArray *navControllers = [NSMutableArray arrayWithCapacity:viewControllers.count];
+	
+	for (NSInteger i = 0; i < viewControllers.count; i++) {
+		UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:viewControllers[i]];
+		navController.tabBarItem = [[UITabBarItem alloc] initWithTitle:tabBarTitles[i] image:nil tag:i];
+		navControllers[i] = navController;
+	}
+	
+	// tab bar controller
+	_tabController = [[UITabBarController alloc] init];
+	[_tabController setViewControllers:navControllers animated:YES];
+	
+	[self.window setRootViewController:_tabController];
+	
     self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+	[self.window makeKeyAndVisible];
+	
     return YES;
 }
 
