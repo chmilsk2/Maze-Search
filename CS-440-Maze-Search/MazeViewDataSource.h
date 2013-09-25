220fa49f1a48653bb2df3c8e84b5eb1816455994
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 17:38:38 -0500

    protocol for updating the view

---
+++ CS-440-Maze-Search/MazeViewDataSource.h
@@ -0,0 +1,21 @@
+//
+//  MazeViewDataSource.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/24/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MazeViewDataSource <NSObject>
+
+- (CGFloat)horizontalMarginForBoardWidth:(CGFloat)width;
+- (CGFloat)verticalMarginForBoardHeight:(CGFloat)height;
+- (CGFloat)width;
+- (CGFloat)height;
+- (NSUInteger)numberOfCols;
+- (NSUInteger)numberOfRows;
+- (NSUInteger)stateForRow:(NSUInteger)row col:(NSUInteger)col;
+
+@end
