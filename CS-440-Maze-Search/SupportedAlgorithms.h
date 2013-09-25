859f797f48fe0599851f720934386f4eaaa2c058
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 17:37:32 -0500

    array of supported algorithm names

---
+++ CS-440-Maze-Search/SupportedAlgorithms.h
@@ -0,0 +1,9 @@
+//
+//  SupportedAlgorithms.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/24/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+NSArray * SupportedAlgorithms();
---
+++ CS-440-Maze-Search/SupportedAlgorithms.m
@@ -0,0 +1,13 @@
+//
+//  SupportedAlgorithms.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/24/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "SupportedAlgorithms.h"
+
+NSArray * SupportedAlgorithms() {
+	return @[@"Depth-First Search", @"Breadth-First Search", @"Greedy Best-First Search", @"A* Search"];
+}
