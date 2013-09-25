3cc5bcca5d94f8ced35bdf9bdb31b40ef842db59
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 17:37:59 -0500

    view controller for other sections

--- CS-440-Maze-Search/SearchWithMultipleDotsViewController.m
+++ CS-440-Maze-Search/SearchWithMultipleDotsViewController.m
@@ -7,31 +7,12 @@
 //
 
 #import "SearchWithMultipleDotsViewController.h"
-#import "MazeManager.h"
-
-#define TAB_TITLE @"1.3"
-#define MAZE_PARSING_NOTIFICATION_NAME @"MazeParsingNotification"
 
 @interface SearchWithMultipleDotsViewController ()
 
 @end
 
-@implementation SearchWithMultipleDotsViewController {
-	NSArray *_mazes;
-}
-
-- (id)init {
-	self = [super init];
-	
-	if (self) {
-		// observe for maze parsing notifications
-		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updatedMazes:) name:MAZE_PARSING_NOTIFICATION_NAME object:nil];
-		
-		_mazes = [MazeManager sharedMazeManager].mazes;
-	}
-	
-	return self;
-}
+@implementation SearchWithMultipleDotsViewController
 
 - (void)viewDidLoad
 {
@@ -41,22 +22,11 @@
 	[self.view setBackgroundColor:[UIColor grayColor]];
 }
 
-#pragma mark - Updated Mazes Notification
-
-- (void)updatedMazes:(NSNotification *)notification {
-	NSNumber *updatedMazeIndex = [notification.userInfo objectForKey:@"updatedMazeIndex"];
-	// all entries in the list view should start off grayed out and remain untappable until the notification is received
-	_mazes = [notification.userInfo objectForKey:@"mazes"];
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)dealloc {
-	[[NSNotificationCenter defaultCenter] removeObserver:self];
-}
 
 @end
