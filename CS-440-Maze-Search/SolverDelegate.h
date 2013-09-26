131efa39dadd016263e9e09c9bc7f17a117d1a44
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 21:00:23 -0500

    Notify delegate when a step has been taken in the algorithm

---
+++ CS-440-Maze-Search/SolverDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SolverDelegate.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SolverDelegate <NSObject>
+
+- (void)tookStep;
+
+@end
