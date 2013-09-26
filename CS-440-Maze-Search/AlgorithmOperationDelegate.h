2aefa6123ce165160319668b03fff1a2372b356a
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 20:54:37 -0500

    Notify Delegate whenever a step has been taken in the algorithm process

---
+++ CS-440-Maze-Search/AlgorithmOperationDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AlgorithmOperationDelegate.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AlgorithmOperationDelegate <NSObject>
+
+- (void)tookStep;
+
+@end
