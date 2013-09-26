c7e5b383ee98dbe4ef518f35fb7eb6f3cf540fd8
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 20:57:30 -0500

    Breadth-First Search Operation

---
+++ CS-440-Maze-Search/BreadthFirstSearchOperation.h
@@ -0,0 +1,14 @@
+//
+//  BreadthFirstSearchOperation.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SearchAlgorithmOperation_Protected.h"
+
+@interface BreadthFirstSearchOperation : SearchAlgorithmOperation
+
+@end
---
+++ CS-440-Maze-Search/BreadthFirstSearchOperation.m
@@ -0,0 +1,17 @@
+//
+//  BreadthFirstSearchOperation.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "BreadthFirstSearchOperation.h"
+
+@implementation BreadthFirstSearchOperation
+
+- (void)main {
+	
+}
+
+@end
