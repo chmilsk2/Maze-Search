9cab45f57411d4bb99e0cee9fb139744c7c12ba3
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 20:58:39 -0500

    Methods accessible only to the subclasses

---
+++ CS-440-Maze-Search/SearchAlgorithmOperation_Protected.h
@@ -0,0 +1,18 @@
+//
+//  SearchAlgorithmOperation_Protected.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "SearchAlgorithmOperation.h"
+
+@interface SearchAlgorithmOperation ()
+
+@property Maze *maze;
+
+- (void)tookStep;
+- (void)didFinish;
+
+@end
