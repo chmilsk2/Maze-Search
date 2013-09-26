ef31b7610b5a8de5a8c8ec1a03e7e2ee40b813d9
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 20:59:02 -0500

    parent class for the various algorithms

---
+++ CS-440-Maze-Search/SearchAlgorithmOperation.h
@@ -0,0 +1,22 @@
+//
+//  SearchAlgorithmOperation.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AlgorithmOperation.h"
+#import "AlgorithmOperationDelegate.h"
+
+@class Maze;
+
+typedef void(^AlgorithmHandler)();
+
+@interface SearchAlgorithmOperation : NSOperation <AlgorithmOperation>
+
+@property (weak) id <AlgorithmOperationDelegate> delegate;
+@property (copy) AlgorithmHandler algorithmCompletionHandler;
+
+@end
---
+++ CS-440-Maze-Search/SearchAlgorithmOperation.m
@@ -0,0 +1,28 @@
+//
+//  SearchAlgorithmOperation.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "SearchAlgorithmOperation_Protected.h"
+#import "Maze.h"
+
+@implementation SearchAlgorithmOperation
+
+- (void)main {
+	// no-op
+}
+
+- (void)didFinish {
+	if (self.algorithmCompletionHandler) {
+		self.algorithmCompletionHandler();
+	}
+}
+
+- (void)tookStep {
+	[self.delegate tookStep];
+}
+
+@end
