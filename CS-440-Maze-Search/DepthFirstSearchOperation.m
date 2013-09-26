5b1ff050eb9a388ea7a5aa8e0f6176d2481b1f37
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-25 20:57:38 -0500

    Depth First Search Operation

---
+++ CS-440-Maze-Search/DepthFirstSearchOperation.m
@@ -0,0 +1,18 @@
+//
+//  DepthFirstSearchOperation.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/25/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "DepthFirstSearchOperation.h"
+
+@implementation DepthFirstSearchOperation
+
+- (void)main {
+	NSLog(@"perform depth first search here");
+	[self didFinish];
+}
+
+@end
