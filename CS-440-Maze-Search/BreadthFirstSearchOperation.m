2003c06a7b50ca0289ccff7c97d8d0c88cc4ae42
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-28 09:54:58 -0500

    breadth first search operation

--- CS-440-Maze-Search/BreadthFirstSearchOperation.m
+++ CS-440-Maze-Search/BreadthFirstSearchOperation.m
@@ -7,10 +7,48 @@
 //
 
 #import "BreadthFirstSearchOperation.h"
+#import "Queue.h"
 
 @implementation BreadthFirstSearchOperation
 
 - (void)main {
+	NSLog(@"perform breadth first search here");
+	
+	[self breadthFirstSearch];
+	
+	[self didFinish];
+}
+
+- (void)breadthFirstSearch {
+	Cell *startingCell = self.maze.startingCell;
+	Cell *goalCell = self.maze.goalCell;
+	
+	NSUInteger startingCol = startingCell.coordinate.x;
+	NSUInteger startingRow = startingCell.coordinate.y;
+	
+	NSUInteger goalCol = goalCell.coordinate.x;
+	NSUInteger goalRow = goalCell.coordinate.y;
+	
+	if (startingCell == goalCell) {
+		// goal has been reached
+	}
+	
+	Queue *frontier = [[Queue alloc] init];
+	NSMutableDictionary *explored = [NSMutableDictionary dictionary];
+	
+	// add the starting cell to the frontier
+	[frontier enqueue:startingCell];
+	
+	// if empty, return failure
+	if (!frontier.count) {
+		// failure
+	}
+	
+	// choose the shallowest cell in the frontier
+	Cell *cell = [frontier dequeue];
+	
+	// add cell to explored
+	[explored setObject:cell forKey:[NSNumber numberWithInteger:cell.hash]];
 	
 }
 
