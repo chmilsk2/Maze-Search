34cebe598edad56ed6fe77329122e536db7aa428
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 15:18:43 -0500

    added None to cost function names

--- CS-440-Maze-Search/CostFunctionNames.h
+++ CS-440-Maze-Search/CostFunctionNames.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
 //
 
+#define NONE @"None"
 #define MANHATTAN_DISTANCE_COST_FUNCTION_NAME @"Manhattan Distance"
 #define C1_COST_FUNCTION_NAME @"C1"
 #define C2_COST_FUNCTION_NAME @"C2"
