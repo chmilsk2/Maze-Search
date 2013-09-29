761e815c0c3826df99aa351eb77f5e04010887c4
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 11:06:31 -0500

    #defines of supported algorithm names

---
+++ CS-440-Maze-Search/SupportedAlgorithmNames.h
@@ -0,0 +1,14 @@
+//
+//  SupportedAlgorithmNames.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define DEPTH_FIRST_SEARCH @"Depth-First Search"
+#define BREADTH_FIRST_SEARCH @"Breadth-First Search"
+#define GREEDY_BEST_FIRST_SEARCH @"Greedy Best-First Search"
+#define A_STAR_SEARCH @"A* Search"
