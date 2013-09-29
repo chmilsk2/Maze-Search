d6c273b7e6c1be52a99fca5ec856fd989f10df65
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 11:08:14 -0500

    moved #defines to separate file

--- CS-440-Maze-Search/SupportedAlgorithms.m
+++ CS-440-Maze-Search/SupportedAlgorithms.m
@@ -7,11 +7,7 @@
 //
 
 #import "SupportedAlgorithms.h"
-
-#define DEPTH_FIRST_SEARCH @"Depth-First Search"
-#define BREADTH_FIRST_SEARCH @"Breadth-First Search"
-#define GREEDY_BEST_FIRST_SEARCH @"Greedy Best-First Search"
-#define A_STAR_SEARCH @"A* Search"
+#import "SupportedAlgorithmNames.h"
 
 NSArray * SupportedAlgorithms() {
 	return @[DEPTH_FIRST_SEARCH, BREADTH_FIRST_SEARCH, GREEDY_BEST_FIRST_SEARCH, A_STAR_SEARCH];
