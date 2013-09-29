8c0ba5b030f92c7ffe15d1e8ebf82a3b5472aaf9
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 10:52:24 -0500

    Added a costFunctionBlock to pass into various algorithm operations

---
+++ CS-440-Maze-Search/CostFunctionBlock.h
@@ -0,0 +1,9 @@
+//
+//  CostFunctionBlock.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+typedef CGFloat(^CostFunctionBlock)(CGPoint point, ...);
