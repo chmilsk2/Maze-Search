9c1f54913d9785a0d57f5b6fd91fa0ca22cd3fdd
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 10:51:30 -0500

    Added A* Search Operation

---
+++ CS-440-Maze-Search/AStarSearchOperation.h
@@ -0,0 +1,14 @@
+//
+//  AStarSearchOperation.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SearchAlgorithmOperation.h"
+
+@interface AStarSearchOperation : SearchAlgorithmOperation
+
+@end
---
+++ CS-440-Maze-Search/AStarSearchOperation.m
@@ -0,0 +1,13 @@
+//
+//  AStarSearchOperation.m
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import "AStarSearchOperation.h"
+
+@implementation AStarSearchOperation
+
+@end
