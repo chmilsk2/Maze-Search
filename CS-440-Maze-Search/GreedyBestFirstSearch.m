7a51115aea9b96d8fbc2ed2251c4c133d08d57d1
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 15:26:26 -0500

    Implemented GreedyBestFirstSearch algorithm

--- CS-440-Maze-Search/GreedyBestFirstSearch.m
+++ CS-440-Maze-Search/GreedyBestFirstSearch.m
@@ -18,10 +18,54 @@
 }
 
 - (void)greedyBestFirstSearch {
-	CGPoint point1 = CGPointMake(1, 1);
-	CGPoint point2 = CGPointMake(5, 5);
-	CGFloat result1FromBlock = self.costFunctionBlock(point1, point2);
-	CGFloat result2FromBlock = self.costFunctionBlock(point1);
+	// use a priority queue
+	
+	Cell *startingCell = self.maze.startingCell;
+	Cell *goalCell = self.maze.goalCell;
+	
+	if ([startingCell isEqual:goalCell]) {
+		// goal reached
+		[startingCell setVisited:YES];
+		[self.delegate tookStep];
+		return;
+	}
+	
+	Frontier *frontier = [[Frontier alloc] init];
+	NSMutableSet *explored = [NSMutableSet set];
+	
+	// add the starting cell to the frontier
+	[frontier enqueueObject:startingCell];
+	
+	while (frontier.count) {
+		// bfs uses a queue
+		// choose the shallowest cell in the frontier
+		Cell *cell = [frontier dequeueObjectWithLowestCostForBlock:self.costFunctionBlock goalPoint:goalCell.coordinate];
+		
+		BOOL goalReached = [cell isEqual:goalCell];
+		
+		// add cell to explored
+		[explored addObject:cell];
+		[cell setVisited:YES];
+		
+		[self.delegate tookStep];
+		
+		if (goalReached) {
+			return;
+		}
+		
+		// look at the child cells
+		NSArray *children = [self.maze childrenForParent:cell];
+		
+		for (Cell *child in children) {
+			// if not in explored or frontier
+			if (![explored containsObject:child] && ![frontier containsObject:child]) {
+				[frontier enqueueObject:child];
+			}
+		}
+	}
+	
+	// frontier is empty, no goal found, return failure
+	return;
 }
 
 @end
