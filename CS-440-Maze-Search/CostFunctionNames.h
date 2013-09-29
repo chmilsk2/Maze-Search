2a065828ca182ffda05c2d8afd470accf8ec7ca6
Troy Chmieleski <chmilsk2@illinois.edu>
2013-09-29 10:52:48 -0500

    #defines for Cost Function Names

---
+++ CS-440-Maze-Search/CostFunctionNames.h
@@ -0,0 +1,12 @@
+//
+//  CostFunctionNames.h
+//  CS-440-Maze-Search
+//
+//  Created by Troy Chmieleski on 9/28/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#define MANHATTAN_DISTANCE_COST_FUNCTION_NAME @"Manhattan Distance"
+#define C1_COST_FUNCTION_NAME @"C1"
+#define C2_COST_FUNCTION_NAME @"C2"
+
